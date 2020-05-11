d81255a93699114636478a14911f811158e6e600
Kweku7k <nkba@live.com>
2020-05-11 11:33:53 +0000

    3 files modified, 3 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class CartService {
@@ -16,6 +17,12 @@ clearCart(){
     this.items = [];
     return this.items;
 }
-  constructor() { }
+
+getShippingPrices(){
+  return this.http.get('/assets/shipping.json')
+}
+  constructor(
+    private http: HttpClient
+  ) { }
 
 }
